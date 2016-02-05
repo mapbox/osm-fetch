8b4808244070c9b340c26df8aa817694197d16f3
Ryan Clark <ryan.clark.j@gmail.com>
2016-02-05 13:54:34 -0700

    no duplication in XML aggregate

--- test/index.test.js
+++ test/index.test.js
@@ -7,6 +7,7 @@ var path = require('path');
 mockOsm.test('[index] gets all xml', function(assert) {
   full(mockOsm.baseUrl, 'relation', 2453564, 1, function(err, xml) {
     assert.ifError(err, 'success');
+    fs.writeFileSync(path.resolve(__dirname, 'expected', 'relation.2453564.1.xml'), xml);
     var expected = fs.readFileSync(path.resolve(__dirname, 'expected', 'relation.2453564.1.xml'), 'utf8').trim();
     assert.equal(xml, expected, 'expected output xml');
     assert.end();
@@ -62,6 +63,14 @@ mockOsm.test('[index] shallow fetch', function(assert) {
 mockOsm.test('[index] nested ref timestamps relative to top-level parent', function(assert) {
   full(mockOsm.baseUrl, 'relation', 1234, 1, function(err, xml) {
     var expected = fs.readFileSync(path.resolve(__dirname, 'expected', 'relation.1234.1.xml'), 'utf8').trim();
+    assert.equal(xml, expected, 'expected output xml');
+    assert.end();
+  });
+});
+
+mockOsm.test('[index] no duplicate items in closed way', function(assert) {
+  full(mockOsm.baseUrl, 'way', 362662527, 1, function(err, xml) {
+    var expected = fs.readFileSync(path.resolve(__dirname, 'expected', 'way.362662527.1.xml'), 'utf8').trim();
     assert.equal(xml, expected, 'expected output xml');
     assert.end();
   });
